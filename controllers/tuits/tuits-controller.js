0649f15258b7e8eeb5e05f95d5a80cb60d0fb85f
jdunlevy15 <jdunlevy15@gmail.com>
2023-04-09 18:05:17 -0400

    a9 branch

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -11,6 +11,10 @@ const createTuit = async (req, res) => {
   const newTuit = req.body;
   newTuit.likes = 0;
   newTuit.liked = false;
+  newTuit.replies = 0;
+  newTuit.retuits = 0;
+  newTuit.dislikes = 0;
+  newTuit.disliked = false;
   const insertedTuit = await tuitsDao.createTuit(newTuit);
   res.json(insertedTuit);
 };
