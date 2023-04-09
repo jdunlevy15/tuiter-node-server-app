d1db9d50c4814a4fc3c7121bfe3aff05d6c852bb
jdunlevy15 <jdunlevy15@gmail.com>
2023-04-09 00:35:18 -0400

    add tuit fields to schema

---
+++ controllers/tuits/tuits-schema.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+const schema = mongoose.Schema(
+  {
+    tuit: String,
+    likes: Number,
+    liked: Boolean,
+    topic: String,
+    username: String,
+    handle: String,
+    time: String,
+    image: String,
+    title: String,
+    replies: Number,
+    retuits: Number,
+    disliked: Boolean,
+    dislikes: Number,
+  },
+  { collection: "tuits" }
+);
+export default schema;
